b5e57cccb1cc0d94dfbc622be8e428dcf6ffc5ba
sergio-slima <sergio_silvalima@hotmail.com>
2021-04-22 06:50:08 -0300

    tela principal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,13 +6,20 @@ import {
     View
 } from 'react-native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import userImg from '../assets/sergio.png';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 export function Header() {
     return (
         <View style={styles.container}>
+            <View>
+                <Text style={styles.greeting}>Olá,</Text>
+                <Text style={styles.userName}>Sergio</Text>
+            </View>
 
+            <Image source={userImg} style={styles.image} />
         </View>
     )
 }
@@ -24,7 +31,22 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         alignItems: 'center',
         paddingVertical: 20,
-        marginTop: getStatusBarHeight(),
-        backgroundColor: colors.red
+        marginTop: getStatusBarHeight()
+    },
+    greeting: {
+        fontSize: 32,
+        color: colors.heading,
+        fontFamily: fonts.text
+    },
+    userName: {
+        fontSize: 32,
+        fontFamily: fonts.heading,
+        color: colors.heading,
+        lineHeight: 40
+    },
+    image: {
+        width: 70,
+        height: 70,
+        borderRadius: 40
     }
 })
