5bd41230b9877a523f39cf40766006255a49e083
sergio-slima <sergio_silvalima@hotmail.com>
2021-06-17 06:14:03 -0300

    usando changeDateTime android e ios

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { 
     StyleSheet,
     Alert,
@@ -13,29 +13,39 @@ import { SvgFromUri } from 'react-native-svg';
 import { Button } from '../components/Button';
 import { getBottomSpace } from 'react-native-iphone-x-helper';
 import { useRoute } from '@react-navigation/core';
+import { format, isBefore } from 'date-fns';
+import { PlantProps } from '../libs/storage';
+import DateTimePicker, { Event } from '@react-native-community/datetimepicker';
 
 import waterdrop from '../assets/waterdrop.png';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 interface Params {
-    plant: {
-        id: string;
-        name: string;
-        about: string;
-        water_tips: string;
-        photo: string;
-        environments: [string];
-        frequency: {
-            times: number;
-            repeat_every: string;
-        }
-    }
+    plant: PlantProps
 }
 
 export function PlantSave() {
+    const [selectedDateTime, setSelectedDateTime] = useState(new Date());
+    const [showDatePicker, setShowDatePicker] = useState(Platform.OS == 'ios');
+    
     const route = useRoute();
     const { plant } = route.params as Params;
+
+    function handleChangeTime(event: Event, dateTime: Date | undefined) {
+        if (Platform.OS === 'android') {
+            setShowDatePicker(oldState => !oldState);
+        }
+        if (dateTime && isBefore(dateTime, new Date())) {
+            setSelectedDateTime(new Date());
+            return Alert.alert('Escolha uma hora no futuro! ')
+        }
+        if (dateTime)
+            setSelectedDateTime(dateTime);
+    }
+    function handleOpenDateTimePickerForAndroid() {
+        setShowDatePicker(oldState => !oldState);
+    }
 
     return (
         <View style={styles.container}>
@@ -66,6 +76,26 @@ export function PlantSave() {
                 <Text style={styles.alertLabel}>
                     Escolha um horário:
                 </Text>
+                {showDatePicker && (
+                    <DateTimePicker
+                        value={selectedDateTime}
+                        mode="time"
+                        display="spinner"
+                        onChange={handleChangeTime}
+                    />                
+                )}
+                {
+                    Platform.OS === 'android' && (
+                        <TouchableOpacity 
+                            style={styles.dateTimePickerButton}
+                            onPress={handleOpenDateTimePickerForAndroid}
+                        >
+                            <Text style={styles.dateTimePickerText}>
+                                {`Mudar ${format(selectedDateTime, 'HH:mm')}`}
+                            </Text>
+                        </TouchableOpacity>
+                    )
+                }
                 <Button
                     title= "Cadastrar planta"
                     onPress={() => {}}
@@ -135,5 +165,15 @@ const styles = StyleSheet.create({
         color: colors.heading,
         fontSize: 12,
         marginBottom: 5
+    },
+    dateTimePickerButton: {
+        width: '100%',
+        alignItems: 'center',
+        paddingVertical: 40
+    },
+    dateTimePickerText: {
+        color: colors.heading,
+        fontSize: 24,
+        fontFamily: fonts.text
     }
 });
--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -11,6 +11,7 @@ import { Header } from '../components/Header';
 import { PlantCardPrimary } from '../components/PlantCardPrimary';
 import { Load } from '../components/Load';
 import { useNavigation } from '@react-navigation/core';
+import { PlantProps } from '../libs/storage';
 
 import api from '../services/api';
 import colors from '../styles/colors';
@@ -19,19 +20,6 @@ import fonts from '../styles/fonts';
 interface EnviromentProps {
     key: string;
     title: string;
-}
-
-interface PlantProps {
-    id: string;
-    name: string;
-    about: string;
-    water_tips: string;
-    photo: string;
-    environments: [string];
-    frequency: {
-        times: number;
-        repeat_every: string;
-    }
 }
 
 export function PlantSelect() {
